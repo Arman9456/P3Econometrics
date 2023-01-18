2eeceb3cec0ddc90f8f0f713a5756b3bbb52b701
Arman9456 <arman.b@live.nl>
2023-01-18 18:00:59 +0100

    jan 18

--- MainCode.R
+++ MainCode.R
@@ -15,24 +15,30 @@ n_y = 5# number of observed variables
 
 eps_hat = matrix(0,nrow=(T-1),ncol=n_y)# initialize empty innovation residual matrix of  dimension (T-1) * n_y  
 
-cov_eps_hat =   # estimated covariance matrices
-eps_hat_c = eps_hat - (1/(T-1))*sum(eps_hat) # (T-1)*n_y matrix of centered residuals
+cov_eps_hat =   # estimated covariance matrix at time t of dimension n_y x n_y 
+eps_hat_c =  matrix(0,nrow=(T-1),ncol=n_y) # (T-1)*n_y matrix of centered residuals
 
-sigma_epsilon_hat_sqrt_inv = inv(sqrtm(cov_eps_hat))
+sigma_epsilon_hat_sqrt_inv =  inv(sqrtm(cov_eps_hat)) # list of dimension (T-1) x  (n_y x n_y) so T-1 matrices of dimension (n_y x n_y)
 
-res_hat = sigma_epsilon_hat_sqrt_inv * eps_hat_c # vector of standardized residuals from index 1 to T-1 which are supposed to be t=2 to T
+res_hat = sigma_epsilon_hat_sqrt_inv * eps_hat_c 
 
+eps_hat_sum = t(rep(0,T)) # 1x n_y vector of sums over time
+
+# fill up eps_hat and sum over time to get centered residuals
 for(i in 1:T-1){
-  eps_hat[,i+1] = y[,i+1] - C %*% Z_hat[...] # we start from index i+1 for eps_hat since we need to start at eps_hat_2
-}
+  eps_hat[i+1,] = y[i+1,] - C %*% Z_hat[...] # we start from index i+1 for eps_hat since we need to start at eps_hat_2
+  eps_hat_sum = eps_hat_sum + eps_hat[i+1,] # for each time T, we add the 1xn_y vectors until we get a 1xn_y vector of the sum
+  }
 
+# get centered residuals and standardized innovations
 for(i in 1:T-1){
-  eps_hat_c[,i+1] = eps_hat[,i+1] - (1/(T-1))*sum(eps_hat[,i+1])
+  eps_hat_c[i+1,] = eps_hat[i+1,] - (1/(T-1))*eps_hat_sum # we make the centered residuals
+  res_hat = sigma_epsilon_hat_sqrt_inv[i+1[]] %*% t(eps_hat_c[i+1,]) # should be multiplication of n_y x n_y matrix with n_y x 1 eps_hat_c[i+1,] for each t
 }
 
 
 for (i in 1:B){
-  star_indices = ceiling(runif(n=T-1,min=1,max=T-1)) #draw indices with replacement from original sample (t=2,...t=T is equivalent to t=1,...,t=T-1 in our case)
+  star_indices = ceiling(runif(n=T-1,min=2,max=T)) #draw indices with replacement from original sample 
   e_star = res_hat[star_indices] # draw with replacement from sample to optain bootstrap standardized innovations
   
   # initialize starting values###
