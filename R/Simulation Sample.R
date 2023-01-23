8091f3015e9c472ec9a3df9c0906974a233b8e2c
Arman9456 <arman.b@live.nl>
2023-01-23 12:39:34 +0100

    Constructing monte carlo samples

---
+++ R/Simulation Sample.R
@@ -0,0 +1,12 @@
+#### Constructing Monte Carlo Samples ######
+
+# Coefficients cycle AR(3) equation
+c1= 0.1
+c2 = 0.25
+c3 = 0.8
+
+# initial values
+cyc_0 = rnorm(1,mean=0,sd=1)
+alpha_0 = rnorm(1,mean=0,sd=1)
+mu_0 = rnorm(1,mean=0,sd=1)
+y_0 = rnorm(1,mean=0,sd=1)
